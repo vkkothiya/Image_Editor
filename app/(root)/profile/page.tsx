8bab6990a56f43209f2341bb5148cd601cf0924d
vkkothiya <viharkothiya003@gmail.com>
2024-05-08 11:38:54 +0530

    userId

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -14,9 +14,9 @@ const Profile = async ({ searchParams }: SearchParamProps) => {
   // if (!userId) redirect("/sign-in");
 
   // const user = await getUserById(userId);
-  const images = await getUserImages({ page, 
-    // userId: user._id 
-  });
+  // const images = await getUserImages({ page, 
+  //   // userId: user._id 
+  // });
 
   return (
     <>
@@ -50,17 +50,17 @@ const Profile = async ({ searchParams }: SearchParamProps) => {
               height={50}
               className="size-9 md:size-12"
             />
-            <h2 className="h2-bold text-dark-600">{images?.data.length}</h2>
+            {/* <h2 className="h2-bold text-dark-600">{images?.data.length}</h2> */}
           </div>
         </div>
       </section>
 
       <section className="mt-8 md:mt-14">
-        <Collection
+        {/* <Collection
           images={images?.data}
           totalPages={images?.totalPages}
           page={page}
-        />
+        /> */}
       </section>
     </>
   );
